64b4308fda4bc3b66a72d5308e370cab21a55824
MahdiOR <mahdi.ostadrahim@gmail.com>
2026-04-07 15:56:21 +0330

    fix: latestschema

--- store/sqlstore/upgrades/00-latest-schema.sql
+++ store/sqlstore/upgrades/00-latest-schema.sql
@@ -126,7 +126,7 @@ CREATE TABLE whatsmeow_privacy_tokens (
 );
 
 CREATE INDEX idx_whatsmeow_privacy_tokens_our_jid_timestamp
-ON whatsmeow_privacy_tokens (our_jid, timestamp);
+ON whatsmeow_privacy_tokens (our_jid, timestamp_info);
 
 CREATE TABLE whatsmeow_lid_map (
     lid NVARCHAR(300) PRIMARY KEY,
@@ -144,15 +144,15 @@ CREATE TABLE whatsmeow_event_buffer (
 );
 
 CREATE TABLE whatsmeow_retry_buffer (
-	our_jid    TEXT   NOT NULL,
-	chat_jid   TEXT   NOT NULL,
-	message_id TEXT   NOT NULL,
-	format     TEXT   NOT NULL,
-	plaintext  bytea  NOT NULL,
-	timestamp  BIGINT NOT NULL,
+	our_jid    VARCHAR(300)   NOT NULL,
+	chat_jid   VARCHAR(300)   NOT NULL,
+	message_id VARCHAR(200)   NOT NULL,
+	format     VARCHAR(300)   NOT NULL,
+	plaintext  VARBINARY(MAX)  NOT NULL,
+	timestamp_info  BIGINT NOT NULL,
 
 	PRIMARY KEY (our_jid, chat_jid, message_id),
 	FOREIGN KEY (our_jid) REFERENCES whatsmeow_device(jid) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE INDEX whatsmeow_retry_buffer_timestamp_idx ON whatsmeow_retry_buffer (our_jid, timestamp);
+CREATE INDEX whatsmeow_retry_buffer_timestamp_idx ON whatsmeow_retry_buffer (our_jid, timestamp_info);
--- store/sqlstore/upgrades/12-privacy-token-sender-timestamp.sql
+++ store/sqlstore/upgrades/12-privacy-token-sender-timestamp.sql
@@ -3,5 +3,3 @@ ALTER TABLE whatsmeow_privacy_tokens ADD sender_timestamp BIGINT;
 
 CREATE INDEX idx_whatsmeow_privacy_tokens_our_jid_timestamp
 ON whatsmeow_privacy_tokens (our_jid, timestamp_info);
-
-ALTER TABLE whatsapp_message_node ADD CONSTRAINT [FK_whatsapp_message_node_our_jid] FOREIGN KEY ([our_jid]) REFERENCES whatsmeow_device ([jid]) ON DELETE CASCADE;
